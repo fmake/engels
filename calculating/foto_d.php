0de8a768b694697af09516d772278a05c94f2a54
wpt <obuto3@gmail.com>
2013-03-20 22:48:23 +0400

    need class

--- calculating/foto_d.php
+++ calculating/foto_d.php
@@ -3,4 +3,8 @@ $no_right_menu = true;
 $globalTemplateParam->set('no_right_menu', $no_right_menu);
 $breadcrubs = $modul->getBreadCrumbs($modul->id);
 
+$page = !empty($_GET['page']) ? abs((int)$_GET['page']) : 1;
+$limit = $configs->reports_count ? $configs->reports_count : 11; #еще одна под блок, но она пустая итого 12 в блоке.
+
 $fmakeSiteFotoDay = new fmakeSiteFotoDay();
+$fmakeSiteFotoDay->order = "b.rating DESC, a.id"; # rating
