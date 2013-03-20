971c74c91786ff6bc4de83d31dd3b921be7c0355
wpt <obuto3@gmail.com>
2013-03-20 23:08:32 +0400

    test_3 dni

--- calculating/foto_d.php
+++ calculating/foto_d.php
@@ -6,11 +6,25 @@ $globalTemplateParam->set('no_right_menu', $no_right_menu);
 //$page = !empty($_GET['page']) ? abs((int)$_GET['page']) : 1;
 //$limit = $configs->reports_count ? $configs->reports_count : 11; # еще одна под блок, но она пустая итого 12 в блоке.
 
-$id = 000; #id 
-$limit_one = 1; #лимит самых крутых фоток
-$page_one = 1; #страница
+#id 
+$id = 000; 
+#лимит самых самых крутых фоток
+$limit_one = 1; 
+#страница
+$page_one = 1;
+# начало дня 
+$start = strtotime("this day 0:00:00"); 
+# конец дня
+$end = strtotime("this day 23:59:59");
 
 $fmakeSiteFotoDay = new fmakeSiteFotoDay();
-$fmakeSiteFotoDay->order = "b.rating DESC, a.id"; # по rating
+$fmakeSiteFotoDay->order = "b.rating DESC, a.id"; # по ratingu
 
+# массив самых крутых фоток
 $top_item_foto = $fmakeSiteFotoDay->getByPageAdmin($id, $limit_one, $page_one,"a.`file` = 'item_foto_day'",true);
+
+# массив всех фоток на этот день
+$item_foto_this_day = $fmakeSiteFotoDay->getByPageAdmin($id, false, false, 
+	" a.`file` = 'item_foto_day' and b.`date` > $start and b.`date` < $end and b.`active` = '1' ");
+
+#массив фоток с средним рейтингом, типо для меню или еще чего
