91f82fa12c265d0a98cea07d711c464c11492025
wpt <obuto3@gmail.com>
2013-03-22 11:44:35 +0400

    fix

--- js/scripts.js
+++ js/scripts.js
@@ -138,13 +138,13 @@ $(document).ready(function(){
 	});
 
 	$('#tape .niz').live('click', function(){
-		var height; //margit-top 3 элементов
+		var height = 30; //margit-top 3 элементов
 		xajax_TapeWave($('#last_id').html());
 		for (var i = 1; i < 4; i++) {
 			start_item++;
 			end_item++;
-			height = parseInt(height) + parseInt($('#x_tape_item'+end_item).height() + 10);
-		};
+			height = height + parseInt($('#x_tape_item'+end_item).height());
+		}
 		alert(height);
 		$('#tape .news').css({'margin-top': parseInt($('#tape .news').css('margin-top')) - height});
 	});
