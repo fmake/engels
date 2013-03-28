941e3c91a8078e883306e0d416146ca3e885b0ca
wpt2 <obuto3@gmail.com>
2013-03-28 13:37:26 +0300

    csss

--- system_rassilka.php
+++ system_rassilka.php
@@ -14,22 +14,41 @@ session_start();
 		$fmakeSiteUser = new fmakeMail(); 
 		$fmakeRassilka = new fmakeRassilka();
 		$news_obj = new fmakeSiteModule();
-		//$fmakeNews = new fmakeNews();
-		//$num_w = date('w',time());
-		//$time = strtotime("-1 days",time());
-		//echo(date('H:i d.m.Y',$fmakeRassilka->isLastDate()));
-		//if($num_w==1 && $fmakeRassilka->isLastDate()<$time){
-		//$date_new = mktime(0,0,0,date('m',$time),date('d',$time),date('Y',$time));
-		//$date_new = date('H:i d.m.Y',$time);
+		$date = strtotime("today");
+		$date = strtotime("-1 days", $date);
+		$news_obj->order = "b.date DESC, a.id";
+		$items_news_lent = $news_obj->getByPageAdmin(2, false, false,
+			"a.`file` = 'item_news' and b.`date` > {$date}",true);
+		foreach ($items_news_lent as $key => $value) {
+			PrintAr($items_news_lent[$key]['anons']);
+		}
+	}
+	
+	/*
+<?php
+header('Content-type: text/html; charset=utf-8'); 
+
+setlocale(LC_ALL, 'ru_RU.UTF-8');
+mb_internal_encoding('UTF-8');
+
+//ini_set('display_errors',1);
+//error_reporting(7);
+session_start();
+
+	require('./fmake/FController.php');
+	$key = "1o0r2i9f3l8a4m7e56";
+	if($key!=$_GET['key']){
+		$fmakeSiteUser = new fmakeMail(); 
+		$fmakeRassilka = new fmakeRassilka();
+		$news_obj = new fmakeSiteModule();
 		$date = strtotime(date("d.m.Y", time()));
 		$date = strtotime("-1 days",date());
 		$news_obj->order = "b.date DESC, a.id";
 		$items_news_lent = $news_obj->getByPageAdmin(2, false, false,
 			"a.`file` = 'item_news' and b.`date` > '$date'",true);
-		//PrintAr($items_news_lent);
 		foreach ($items_news_lent as $key => $value) {
 			PrintAr($items_news_lent[$key]['anons']);
 		}
-		//}
 	}
 	
+	*/
