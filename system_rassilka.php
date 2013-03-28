4122150b925167260a47667df8daf7d41f8fe0ae
wpt2 <obuto3@gmail.com>
2013-03-28 13:45:19 +0300

    csss

--- system_rassilka.php
+++ system_rassilka.php
@@ -4,7 +4,7 @@ header('Content-type: text/html; charset=utf-8');
 setlocale(LC_ALL, 'ru_RU.UTF-8');
 mb_internal_encoding('UTF-8');
 
-//ini_set('display_errors',1);
+ini_set('display_errors',1);
 error_reporting(7);
 session_start();
 
