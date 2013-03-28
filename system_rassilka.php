27c903a26342fc64507a445c0fa8f0d91846777d
wpt2 <obuto3@gmail.com>
2013-03-28 13:50:13 +0300

    csss

--- system_rassilka.php
+++ system_rassilka.php
@@ -7,9 +7,10 @@ mb_internal_encoding('UTF-8');
 ini_set('display_errors',1);
 error_reporting(7);
 session_start();
-
+	
 	require('./fmake/FController.php');
 	$key = "1o0r2i9f3l8a4m7e56";
+	echo "1";
 	if($key != $_GET['key']){
 		$fmakeSiteUser = new fmakeMail(); 
 		$fmakeRassilka = new fmakeRassilka();
@@ -20,7 +21,7 @@ session_start();
 		$items = $news_obj->getByPageAdmin(2, false, false,
 			"a.`file` = 'item_news' and b.`date` > {$date}",true);
 		foreach ($items as $key => $value) {
-			$items[] = date("d.m.Y", $items_news_lent[$key]['date']));
+			$items[] = date("d.m.Y", $items[$key]['date']));
 		}
 		if($items){
 			$fmakeRassilka->addParam('date',$date_new);
@@ -37,38 +38,7 @@ session_start();
 						
 			$users_all = $fmakeSiteUser->getAllRassilka(true);
 			//echo($_messages['template']);
-			foreach($users_all as $item_user){
-				//echo($item_user['email']."<br/>");
-				$globalTemplateParam->set('item_user', $item_user);
-				
-				$tmp = $twig->getLoader();
-				$twig->setLoader(new Twig_Loader_String());
-				$text = $twig->loadTemplate($_messages['template'])->render($globalTemplateParam->get());
-				$twig->setLoader($tmp);
-				
-				$mail = new PHPMailer(); 
-				$mail->CharSet = "utf-8"; 
-				$mail->From = "info@{$hostname}";
-				$mail->FromName = $hostname;
-				
-				//$mail->AddAddress($item_user['email']);
-				$mail->AddAddress("Obuto3@gmail.com");
-				
-				
-				$mail->WordWrap = 50;                                 
-				$mail->SetLanguage("ru");
 			
-				$mail->IsHTML(true);
-				
-				$mail->Subject = $_messages['title'];
-				$mail->Body    = $text;
-				
-					if(!$mail->Send())
-					{
-					   //echo "Message could not be sent. <p>";
-					   //echo "Mailer Error: " . $mail->ErrorInfo;
-					}
-				}
 			}
 		//}
 	}
