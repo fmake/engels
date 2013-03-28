b40bbbd1931e76a6719f9fa45e52215a0989c8c9
wpt2 <obuto3@gmail.com>
2013-03-28 13:44:56 +0300

    csss

--- system_rassilka.php
+++ system_rassilka.php
@@ -5,26 +5,74 @@ setlocale(LC_ALL, 'ru_RU.UTF-8');
 mb_internal_encoding('UTF-8');
 
 //ini_set('display_errors',1);
-//error_reporting(7);
+error_reporting(7);
 session_start();
 
 	require('./fmake/FController.php');
 	$key = "1o0r2i9f3l8a4m7e56";
-	if($key!=$_GET['key']){
+	if($key != $_GET['key']){
 		$fmakeSiteUser = new fmakeMail(); 
 		$fmakeRassilka = new fmakeRassilka();
 		$news_obj = new fmakeSiteModule();
 		$date = strtotime("today");
 		$date = strtotime("-1 days", $date);
 		$news_obj->order = "b.date DESC, a.id";
-		$items_news_lent = $news_obj->getByPageAdmin(2, false, false,
+		$items = $news_obj->getByPageAdmin(2, false, false,
 			"a.`file` = 'item_news' and b.`date` > {$date}",true);
 		foreach ($items_news_lent as $key => $value) {
-			PrintAr(date("d.m.Y", $items_news_lent[$key]['date']));
+			$items[] = date("d.m.Y", $items_news_lent[$key]['date']));
 		}
+		if($items){
+			$fmakeRassilka->addParam('date',$date_new);
+			$fmakeRassilka->addParam('date_create',time());
+			$fmakeRassilka->addParam('count_item',sizeof($items));
+			$fmakeRassilka->newItem();
+			
+			$fmakeMessages = new fmakeMessages();
+			$fmakeMessages->setId(4);
+			$_messages = $fmakeMessages->getInfo();
+			
+			$globalTemplateParam->set('items', $items);
+			$globalTemplateParam->set('hostname',$hostname);
+						
+			$users_all = $fmakeSiteUser->getAllRassilka(true);
+			//echo($_messages['template']);
+			foreach($users_all as $item_user){
+				//echo($item_user['email']."<br/>");
+				$globalTemplateParam->set('item_user', $item_user);
+				
+				$tmp = $twig->getLoader();
+				$twig->setLoader(new Twig_Loader_String());
+				$text = $twig->loadTemplate($_messages['template'])->render($globalTemplateParam->get());
+				$twig->setLoader($tmp);
+				
+				$mail = new PHPMailer(); 
+				$mail->CharSet = "utf-8"; 
+				$mail->From = "info@{$hostname}";
+				$mail->FromName = $hostname;
+				
+				//$mail->AddAddress($item_user['email']);
+				$mail->AddAddress("Obuto3@gmail.com");
+				
+				
+				$mail->WordWrap = 50;                                 
+				$mail->SetLanguage("ru");
+			
+				$mail->IsHTML(true);
+				
+				$mail->Subject = $_messages['title'];
+				$mail->Body    = $text;
+				
+					if(!$mail->Send())
+					{
+					   //echo "Message could not be sent. <p>";
+					   //echo "Mailer Error: " . $mail->ErrorInfo;
+					}
+				}
+			}
+		//}
 	}
-	
-	/*
+		/*
 <?php
 header('Content-type: text/html; charset=utf-8'); 
 
@@ -41,13 +89,13 @@ session_start();
 		$fmakeSiteUser = new fmakeMail(); 
 		$fmakeRassilka = new fmakeRassilka();
 		$news_obj = new fmakeSiteModule();
-		$date = strtotime(date("d.m.Y", time()));
-		$date = strtotime("-1 days",date());
+		$date = strtotime("today");
+		$date = strtotime("-1 days", $date);
 		$news_obj->order = "b.date DESC, a.id";
 		$items_news_lent = $news_obj->getByPageAdmin(2, false, false,
-			"a.`file` = 'item_news' and b.`date` > '$date'",true);
+			"a.`file` = 'item_news' and b.`date` > {$date}",true);
 		foreach ($items_news_lent as $key => $value) {
-			PrintAr($items_news_lent[$key]['anons']);
+			PrintAr(date("d.m.Y", $items_news_lent[$key]['date']));
 		}
 	}
 	
