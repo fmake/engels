9159076d10138b8a8af11ff65c02e6773861e431
mamay1986 <mamaev.aleksander@gmail.com>
2013-04-01 16:32:05 +0400

    add static templates

--- calculating/mod_vote.php
+++ calculating/mod_vote.php
@@ -7,10 +7,14 @@ $interview = $fmakeInterview->getInterview($count);
 
 $iscookie = array();
 $vopros = array();
+$vopros_statistic_all = array();
 if ($interview) foreach ($interview as $key=>$interview_item) {
 	
 	$fmakeInterview->table = $fmakeInterview->table_vopros;
 	$vopros[$key] = $fmakeInterview->getVoproses($interview_item['id'],true);
+	if($vopros[$key])foreach($vopros[$key] as $k=>$v){
+		$vopros_statistic_all[$key] += $v['stat'];
+	}
 	if($request->interview_id != $interview_item['id']) {
 		$iscookie[$key] = $fmakeInterview->isCookies($interview_item['id']);
 	} else {
@@ -21,6 +25,7 @@ if ($interview) foreach ($interview as $key=>$interview_item) {
 
 $breadcrubs = $modul->getBreadCrumbs($modul->id);
 $globalTemplateParam->set('vopros',$vopros);
+$globalTemplateParam->set('vopros_statistic_all',$vopros_statistic_all);
 $globalTemplateParam->set('interview',$interview);
 $globalTemplateParam->set('iscookie',$iscookie);
 $globalTemplateParam->set('breadcrubs', $breadcrubs);
