73d3b5b9496fb68846ca3da76758bcab3126be13
wpt <obuto3@gmail.com>
2013-04-01 12:08:50 +0400

    tesings

--- admin/modules/mod_news.php
+++ admin/modules/mod_news.php
@@ -34,7 +34,16 @@ $absitem_dop->setId($request->id);
 $mneniya = new fmakeMneniya();
 $older = $absitem_dop ->getAll();
 PrintAr($older);
-foreach ($older as $key => $value) {}
+foreach ($older as $key => $value) {
+	if ($older[$key]['expert'] != 0 or $older[$key]['text_expert'] != 0 or $older[$key]['active_mnenie'] != 0 or $order[$key]['expert_picture'] != 0 ){
+		$mneniya -> addParam('text_expert', $older[$key]['text_expert']);
+		$mneniya -> addParam('expert', $older[$key]['expert']);
+		$mneniya -> addParam('active_mnenie', $older[$key]['active_mnenie']);
+		$mneniya -> addParam('expert_picture', $older[$key]['expert_picture']);
+		$mneniya -> addParam('id_news', $order[$key]['id'])
+		$mneniya -> newItem();
+	}
+}
 $mneniya -> setId($request->id);
 #----------------------------мнения 
 $news_categories = $absitem->getCatAsTree($id_page_modul,0,true);
