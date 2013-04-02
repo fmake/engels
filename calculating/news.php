6c6a497d16611f41cc5993dbfb23c9c2bc8d8c3a
wpt <obuto3@gmail.com>
2013-04-02 10:41:03 +0400

    news

--- calculating/news.php
+++ calculating/news.php
@@ -90,6 +90,7 @@
 				}
 				$exp = $total_exp;
 				$globalTemplateParam->set('exp', $exp);
+								PrintAr($item);
 				//PrintAr($exp);
 				#---------------------------------------------------------------мнения
 				if ($item['dop_params']['templ'] == 1)				
@@ -100,8 +101,7 @@
 				//$news_obj->order = "b.date DESC, a.id";
 				//$news_obj->setRedir($request->modul);
 				//$item = $news_obj->getInfo();
-				
-				PrintAr($item);
+			
 				$cat = $news_obj->getCatForMenu($item[$news_obj->idField],true);
 				$parents = $news_obj->getCats($item[$news_obj->idField]);
 				
