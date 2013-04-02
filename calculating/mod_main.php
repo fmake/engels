63fd464478199805755c0a5b0d015c0191679659
wpt <obuto3@gmail.com>
2013-04-02 11:06:45 +0400

    news

--- calculating/mod_main.php
+++ calculating/mod_main.php
@@ -14,7 +14,8 @@
 	$items_news_lent = $news_obj->getByPageAdmin(2, $limit_news_lent,1,"a.`file` = 'item_news'",true);
 	if ($items_news_lent) foreach ($items_news_lent as $key=>$item) {
 		$items_news_lent[$key]['comment'] = $fmakeComments->getByPageCount($item[$news_obj->idField],true);
-		PrintAr(fmakeNews::getCats($items_news_lent['parent'], true));
+		$fmakeNews = new fmakeNews;
+		PrintAr($fmakeNews -> getCats($items_news_lent['parent'], true));
 	}
 
 	PrintAr($items_news_lent);
