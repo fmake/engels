0f888bbe68f41620efd7a8c620a33605518e101d
wpt <obuto3@gmail.com>
2013-04-02 12:17:24 +0400

    news

--- mneniya_transfer.php
+++ mneniya_transfer.php
@@ -6,6 +6,8 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 require('./fmake/FController.php');
 
+$absitem = new fmakeNews();
+
 $id_page_modul = 2;
 $fmakeTypeTable = new fmakeTypeTable();
 $absitem_dop = new fmakeTypeTable();
@@ -13,9 +15,11 @@ $absitem_dop->table = $fmakeTypeTable->getTable($id_page_modul);
 
 $mneniya = new fmakeMneniya();
 $all_m = $mneniya->getAll();
-$older = $absitem_dop ->getAll();
-//PrintAr($older);
 
+$older = $absitem ->getByPageAdmin(2, false, false, "a.text_expert != '' and a.`expert` != '' ");
+PrintAr($older);
+
+/*
 if($older)foreach ($older as $key => $value) {
 	if ($older[$key]['expert'] != 0 or $older[$key]['text_expert'] != 0 or $older[$key]['active_mnenie'] != 0 or $order[$key]['expert_picture'] != 0 ) {
 		$mneniya -> addParam('text_expert', $older[$key]['text_expert']);
@@ -25,9 +29,9 @@ if($older)foreach ($older as $key => $value) {
 		$mneniya -> addParam('id_news', $older[$key]['id']);
 		$mneniya -> newItem();
 		if ($older[$key]['expert_picture']) {
-			$absitem->addExpertFile($older[$key]['expert_picture'], $older[$key]['expert_picture'], $mneniya->id);
+			$absitem->addExpertFile("{ROOT}/{$absitem->fileDirectory}/{$older[$key][id]}/expert/{$mneniya->id}", $older[$key]['expert_picture'], $mneniya->id);
 		}
 	}
 }
-
+*/
 ?>
