1ff10a6cd75e3a28af862c6c26b16d8ac7d24cd1
wpt <obuto3@gmail.com>
2013-04-02 11:26:06 +0400

    news

--- calculating/mod_main.php
+++ calculating/mod_main.php
@@ -11,7 +11,7 @@
 	}
 	//printar($items_news_main);
 	$limit_news_lent = 13;
-	$items_news_lent = $news_obj->getByPageAdmin(2, $limit_news_lent,1,"a.`file` = 'item_news' and `name_category` = 'Саратов' ",true);
+	$items_news_lent = $news_obj->getByPageAdmin(2, $limit_news_lent,1,"a.`file` = 'item_news' and c.caption name_category = 'Саратов' ",true);
 	if ($items_news_lent) foreach ($items_news_lent as $key=>$item) {
 		$items_news_lent[$key]['comment'] = $fmakeComments->getByPageCount($item[$news_obj->idField],true);
 	}
