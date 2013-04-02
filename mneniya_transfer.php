122ff8a1beb9e75e1c10cca52caead144cd6a4f0
wpt <obuto3@gmail.com>
2013-04-02 12:22:12 +0400

    news

--- mneniya_transfer.php
+++ mneniya_transfer.php
@@ -17,9 +17,9 @@ $mneniya = new fmakeMneniya();
 $all_m = $mneniya->getAll();
 
 $older = $absitem ->getByPageAdmin(2, false, false, "a.`file` = 'item_news' and b.`text_expert` != '' and b.`expert` != '' ");
-PrintAr($older);
+//PrintAr($older);
 
-/*
+
 if($older)foreach ($older as $key => $value) {
 	if ($older[$key]['expert'] != 0 or $older[$key]['text_expert'] != 0 or $older[$key]['active_mnenie'] != 0 or $order[$key]['expert_picture'] != 0 ) {
 		$mneniya -> addParam('text_expert', $older[$key]['text_expert']);
@@ -30,8 +30,9 @@ if($older)foreach ($older as $key => $value) {
 		$mneniya -> newItem();
 		if ($older[$key]['expert_picture']) {
 			$absitem->addExpertFile("{ROOT}/{$absitem->fileDirectory}/{$older[$key][id]}/expert/{$mneniya->id}", $older[$key]['expert_picture'], $mneniya->id);
+			PrintAr("file_create");
 		}
+		PrintAr("+1");
 	}
 }
-*/
 ?>
