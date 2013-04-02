0dfac9089a3049ce37941aca680bddf45c3ec184
wpt <obuto3@gmail.com>
2013-04-02 17:05:11 +0400

    retro

--- fmake/libs/function_xajax.php
+++ fmake/libs/function_xajax.php
@@ -67,8 +67,8 @@ function TapeWaveTab($val){
 
 	$text = $twig->loadTemplate("xajax/TapeWave.tpl")->render($globalTemplateParam->get()); 
 	$objResponse->assign("x_tape", "innerHTML", $text);
-	$script .= "$('#tape .nav ul li .active').remoteClass('active');";
-
+	$script = "$('#tape .nav ul li .active').remoteClass('active');";
+	$objResponse->script($script);
 	return $objResponse;
 }
 
