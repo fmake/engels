dc17b43a3d7e1e1b0ce81535f2dc69a820f8266e
wpt <obuto3@gmail.com>
2013-04-24 17:18:34 +0400

    comment

--- js/jquery.colorbox.js
+++ js/jquery.colorbox.js
@@ -329,7 +329,7 @@
 				$next = $tag(div, "Next"),
 				$prev = $tag(div, "Previous"),
 				$slideshow = $tag(div, "Slideshow").bind(event_open, slideshow),
-				$close = $tag(div, "Close"),
+				$close = $tag(div, "Close")
 			);
 			
 			$wrap.append( // The 3x3 Grid that makes up ColorBox
