72e46ff128e5cc4c349e7685bc736fb0c0b00a86
wpt <obuto3@gmail.com>
2013-05-22 13:36:45 +0400

    skolko onlain na saite

--- fmake/modules/core/fmakeOnline/fmakeOnline.php
+++ fmake/modules/core/fmakeOnline/fmakeOnline.php
@@ -4,7 +4,7 @@ class fmakeOnline extends fmakeSiteModule{
 
     public function getOnlineUser($page_id, $user_time){
         $select = $this->dataBase->SelectFromDB(__LINE__);
-        $where = "id = {$page_id} and `date` >= ({$user_time}-60) and `date` <= {$user_time}";
+        $where = "id!={$page_id} and `date` >= ({$user_time}-60) and `date` <= {$user_time}";
         if($where)
             $select->addWhere($where);
         $result = $select->addFild("COUNT(*)")->addFrom($this->table)->queryDB();
